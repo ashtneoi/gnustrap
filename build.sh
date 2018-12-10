3627e1bf0f2778f8298dd7326865992e68d8d041
ashtneoi <ashtneoi@gmail.com>
2018-12-09 19:30:22 -0800

    Refactor and start working on phase B

--- build.sh
+++ build.sh
@@ -1,32 +1,20 @@
 #!/usr/bin/env bash
 set -eu
 
-THREADS=3
-
-build="$(realpath -m build)"
-build2="$(realpath -m build2)"
-build_root="$(realpath build-root)"
-root="$(realpath -m root)"
-root2="$(realpath -m root2)"
-
-mkdir -p "$root/all"
-
 # build-root build root proj src make_extra [config_option...]
 build() {
-    local build_root="$1"
-    local build="$2"
-    local root="$3"
-    local proj="$4"
-    local src="$5"
-    local make_extra="$6" # watch out!
-    shift 6
+    local proj="$1"
+    local src="$(realpath "$2")"
+    local make_extra="$3" # watch out!
+    shift 3
+
+    p="$build_root/$proj/bin:$newpath"
 
     mkdir -p "$build/$proj"
-    cd "$build/$proj"
-    export PATH="$build_root/$proj/bin:$root/all/bin:$build_root/all/bin"
+    pushd "$build/$proj" >/dev/null
     if ! [[ -e "$build/$proj.configure.stamp" ]]; then
         rm -f "$root/$proj.stamp"
-        ../../$src/configure \
+        PATH="$p" "$src/configure" \
             --build="$arch_build" \
             --host="$arch_host" \
             --prefix="$root/all" \
@@ -35,38 +23,49 @@ build() {
     fi
     if ! [[ -e "$root/$proj.stamp" ]]; then
         echo "### $proj ###"
-        make -j $THREADS $make_extra
-        make install $make_extra
+        PATH="$p" make -j $THREADS $make_extra
+        PATH="$p" make install $make_extra
         touch "$root/$proj.stamp"
     fi
+    popd >/dev/null
 }
 
-mkdir -p "$root/all"
+THREADS=3
+
+build_root="$(realpath build-root)"
+
+build="$(realpath -m build)"
+root="$(realpath -m root)"
+newpath="$root/all/bin:$build_root/all/bin"
 arch_build=x86_64-linux-gnu
 arch_host=x86_64-linux-gnu
 arch_target=$arch_host
 
-build $build_root $build $root grep grep-3.1 ""
-build $build_root $build $root make make-4.2 ""
-build $build_root $build $root binutils binutils-2.31 "MAKEINFO=true" \
+build grep grep-3.1 ""
+build make make-4.2 ""
+build binutils binutils-2.31 "MAKEINFO=true" \
     --target=$arch_target --disable-multilib
-build $build_root $build $root bash bash-4.4 ""
+build bash bash-4.4 ""
 ln -fs bash $root/all/bin/sh
-build $build_root $build $root gcc gcc-8.2.0 "" \
+build gcc gcc-8.2.0 "" \
     --target=$arch_target --enable-languages=c,c++,lto --disable-multilib \
     --disable-bootstrap
-build $build_root $build $root gawk gawk-4.2.1 ""
-build $build_root $build $root m4 m4-1.4.18 ""
-build $build_root $build $root bison bison-3.2 ""
-build $build_root $build $root gzip gzip-1.9 ""
-build $build_root $build $root glibc glibc-2.28 "" \
+build gawk gawk-4.2.1 ""
+build m4 m4-1.4.18 ""
+build bison bison-3.2 ""
+build gzip gzip-1.9 ""
+build glibc glibc-2.28 "" \
     --target=$arch_target --disable-multi-arch
 
-mkdir -p "$root2/all"
+build="$(realpath -m build2)"
+root="$(realpath -m root2)"
+newpath="$root/all/bin:$newpath"
+arch_build=x86_64-linux-gnu
+arch_host=x86_64-linux-gnu
 arch_target=avr
 
-build $build_root $build2 $root2 binutils binutils-2.31 "MAKEINFO=true" \
+build binutils binutils-2.31 "MAKEINFO=true" \
     --target=$arch_target --disable-multilib
-build $build_root $build $root gcc gcc-8.2.0 "" \
+build gcc gcc-8.2.0 "" \
     --target=$arch_target --enable-languages=c,c++,lto --disable-multilib \
     --disable-bootstrap
