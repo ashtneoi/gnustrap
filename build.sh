89b5d1dcd9b049231025d8cbc3839e8a4ccf8289
ashtneoi <ashtneoi@gmail.com>
2018-12-09 18:20:02 -0800

    idk

--- build.sh
+++ build.sh
@@ -11,226 +11,62 @@ root2="$(realpath -m root2)"
 
 mkdir -p "$root/all"
 
-mkdir -p "$build/grep"
-cd "$build/grep"
-export PATH="$build_root/grep/bin:$build_root/all/bin"
-if ! [[ -e "$build/grep.configure.stamp" ]]; then
-    rm -f "$root/grep.stamp"
-    ../../grep-3.1/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/grep.configure.stamp"
-fi
-if ! [[ -e "$root/grep.stamp" ]]; then
-    echo "### grep ###"
-    make -j $THREADS
-    make install
-    touch "$root/grep.stamp"
-fi
-prevroot="$root/all/bin"
+# build-root build root proj src make_extra [config_option...]
+build() {
+    local build_root="$1"
+    local build="$2"
+    local root="$3"
+    local proj="$4"
+    local src="$5"
+    local make_extra="$6" # watch out!
+    shift 6
 
-mkdir -p "$build/make"
-cd "$build/make"
-export PATH="$build_root/make/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/make.configure.stamp" ]]; then
-    echo "### make ###"
-    rm -f "$root/make.stamp"
-    ../../make-4.2/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/make.configure.stamp"
-fi
-if ! [[ -e "$root/make.stamp" ]]; then
-    echo "### make ###"
-    make -j $THREADS
-    make install
-    touch "$root/make.stamp"
-fi
+    mkdir -p "$build/$proj"
+    cd "$build/$proj"
+    export PATH="$build_root/$proj/bin:$root/all/bin:$build_root/all/bin"
+    if ! [[ -e "$build/$proj.configure.stamp" ]]; then
+        rm -f "$root/$proj.stamp"
+        ../../$src/configure \
+            --build="$arch_build" \
+            --host="$arch_host" \
+            --prefix="$root/all" \
+            "$@"
+        touch "$build/$proj.configure.stamp"
+    fi
+    if ! [[ -e "$root/$proj.stamp" ]]; then
+        echo "### $proj ###"
+        make -j $THREADS $make_extra
+        make install $make_extra
+        touch "$root/$proj.stamp"
+    fi
+}
 
-mkdir -p "$build/binutils"
-cd "$build/binutils"
-export PATH="$build_root/binutils/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/binutils.configure.stamp" ]]; then
-    echo "### binutils ###"
-    rm -f "$root/binutils.stamp"
-    ../../binutils-2.31/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --target=x86_64-linux-gnu \
-        --disable-multilib \
-        --prefix="$root/all"
-    touch "$build/binutils.configure.stamp"
-fi
-if ! [[ -e "$root/binutils.stamp" ]]; then
-    echo "### binutils ###"
-    make -j $THREADS MAKEINFO=true
-    make install MAKEINFO=true
-    touch "$root/binutils.stamp"
-fi
-prevroot="$root/all/x86_64-linux-gnu/bin:$prevroot"
+mkdir -p "$root/all"
+arch_build=x86_64-linux-gnu
+arch_host=x86_64-linux-gnu
+arch_target=$arch_host
 
-mkdir -p "$build/bash"
-cd "$build/bash"
-export PATH="$build_root/bash/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/bash.configure.stamp" ]]; then
-    echo "### bash ###"
-    rm -f "$root/bash.stamp"
-    ../../bash-4.4/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/bash.configure.stamp"
-fi
-if ! [[ -e "$root/bash.stamp" ]]; then
-    echo "### bash ###"
-    make -j $THREADS
-    make install
-    cd "$root/all/bin"
-    ln -fs bash sh
-    touch "$root/bash.stamp"
-fi
-
-mkdir -p "$build/gcc"
-cd "$build/gcc"
-export PATH="$build_root/gcc/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/gcc.configure.stamp" ]]; then
-    echo "### gcc ###"
-    rm -f "$root/gcc.stamp"
-    ../../gcc-8.2.0/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --target=x86_64-linux-gnu \
-        --enable-languages=c,c++,lto \
-        --disable-multilib \
-        --disable-bootstrap \
-        --prefix="$root/all"
-    touch "$build/gcc.configure.stamp"
-fi
-if ! [[ -e "$root/gcc.stamp" ]]; then
-    echo "### gcc ###"
-    make -j $THREADS #"LDFLAGS=-lstdc++"
-    make install
-    touch "$root/gcc.stamp"
-fi
-
-mkdir -p "$build/gawk"
-cd "$build/gawk"
-export PATH="$build_root/gawk/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/gawk.configure.stamp" ]]; then
-    echo "### gawk ###"
-    rm -f "$root/gawk.stamp"
-    ../../gawk-4.2.1/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/gawk.configure.stamp"
-fi
-if ! [[ -e "$root/gawk.stamp" ]]; then
-    echo "### gawk ###"
-    make -j $THREADS
-    make install
-    touch "$root/gawk.stamp"
-fi
-
-mkdir -p "$build/m4"
-cd "$build/m4"
-export PATH="$build_root/m4/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/m4.configure.stamp" ]]; then
-    echo "### m4 ###"
-    rm -f "$root/m4.stamp"
-    ../../m4-1.4.18/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/m4.configure.stamp"
-fi
-if ! [[ -e "$root/m4.stamp" ]]; then
-    echo "### m4 ###"
-    make -j $THREADS
-    make install
-    touch "$root/m4.stamp"
-fi
-
-mkdir -p "$build/bison"
-cd "$build/bison"
-export PATH="$build_root/bison/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/bison.configure.stamp" ]]; then
-    echo "### bison ###"
-    rm -f "$root/bison.stamp"
-    ../../bison-3.2/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/bison.configure.stamp"
-fi
-if ! [[ -e "$root/bison.stamp" ]]; then
-    echo "### bison ###"
-    make -j $THREADS
-    make install
-    touch "$root/bison.stamp"
-fi
-
-mkdir -p "$build/gzip"
-cd "$build/gzip"
-export PATH="$build_root/gzip/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/gzip.configure.stamp" ]]; then
-    echo "### gzip ###"
-    rm -f "$root/gzip.stamp"
-    ../../gzip-1.9/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/gzip.configure.stamp"
-fi
-if ! [[ -e "$root/gzip.stamp" ]]; then
-    echo "### gzip ###"
-    make -j $THREADS
-    make install
-    touch "$root/gzip.stamp"
-fi
-
-mkdir -p "$build/glibc"
-cd "$build/glibc"
-export PATH="$build_root/glibc/bin:$prevroot:$build_root/all/bin"
-if ! [[ -e "$build/glibc.configure.stamp" ]]; then
-    echo "### glibc ###"
-    rm -f "$root/glibc.stamp"
-    ../../glibc-2.28/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --prefix="$root/all"
-    touch "$build/glibc.configure.stamp"
-fi
-if ! [[ -e "$root/glibc.stamp" ]]; then
-    echo "### glibc ###"
-    make -j $THREADS
-    make install
-    touch "$root/glibc.stamp"
-fi
+build $build_root $build $root grep grep-3.1 ""
+build $build_root $build $root make make-4.2 ""
+build $build_root $build $root binutils binutils-2.31 "MAKEINFO=true" \
+    --target=$arch_target --disable-multilib
+build $build_root $build $root bash bash-4.4 ""
+ln -fs bash $root/all/bin/sh
+build $build_root $build $root gcc gcc-8.2.0 "" \
+    --target=$arch_target --enable-languages=c,c++,lto --disable-multilib \
+    --disable-bootstrap
+build $build_root $build $root gawk gawk-4.2.1 ""
+build $build_root $build $root m4 m4-1.4.18 ""
+build $build_root $build $root bison bison-3.2 ""
+build $build_root $build $root gzip gzip-1.9 ""
+build $build_root $build $root glibc glibc-2.28 "" \
+    --target=$arch_target --disable-multi-arch
 
 mkdir -p "$root2/all"
+arch_target=avr
 
-mkdir -p "$build2/gcc"
-cd "$build2/gcc"
-export PATH="$prevroot:$build_root/all/bin"
-if ! [[ -e "$build2/gcc.configure.stamp" ]]; then
-    echo "### gcc again ###"
-    rm -f "$root2/gcc.stamp"
-    ../../gcc-8.2.0/configure \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --target=x86_64-linux-gnu \
-        --enable-languages=c,c++,lto \
-        --disable-multilib \
-        --disable-bootstrap \
-        --prefix="$root2/all"
-    touch "$build2/gcc.configure.stamp"
-fi
-if ! [[ -e "$root2/gcc.stamp" ]]; then
-    echo "### gcc ###"
-    make -j $THREADS
-    make install
-    touch "$root2/gcc.stamp"
-fi
+build $build_root $build2 $root2 binutils binutils-2.31 "MAKEINFO=true" \
+    --target=$arch_target --disable-multilib
+build $build_root $build $root gcc gcc-8.2.0 "" \
+    --target=$arch_target --enable-languages=c,c++,lto --disable-multilib \
+    --disable-bootstrap
